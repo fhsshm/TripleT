b13269e6aa083ff1d926c6dc8e999a046a038482
fhsshm <fhsshm@gmail.com>
2019-03-03 18:38:36 +0900

    initialized server and css files

---
+++ src/main/java/com/foshimmy/web/TripleTApplication.java
@@ -0,0 +1,13 @@
+package com.foshimmy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleTApplication.class, args);
+	}
+
+}
